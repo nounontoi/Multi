d5a3317ff4bef3ca28a9ef7e10e47a3c96ba856c
TitanPlayz <118145727+TitanPlayz100@users.noreply.github.com>
2024-06-25 10:42:46 +1000

    added different sized icons, used smallest rn

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
 @echo off
 
 @REM This is the compile command
-csc /out:Multi.exe /m:Home /win32icon:icon.ico *.cs
+csc /out:Multi.exe /m:Home /win32icon:icons/multix32.ico *.cs
 
 @REM These are unit testing because why not. Compiles, runs, then deletes the exe
 csc /out:Testing.exe /m:UnitTests *.cs >bin/nul
